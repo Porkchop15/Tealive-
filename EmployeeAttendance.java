23f136583eb96d185ba17603bc45bf5a5d026711
Zachary Michael Hugo A. Reyes <111546468+Porkchop15@users.noreply.github.com>
2024-03-11 13:06:09 +0800

    Create EmployeeAttendance.java

---
+++ EmployeeAttendance.java
@@ -0,0 +1,145 @@
+package src;
+
+import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JTable;
+
+public class EmployeeAttendance extends JFrame {
+
+	private JPanel contentPane;
+	private JTable table;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					EmployeeAttendance frame = new EmployeeAttendance();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public EmployeeAttendance() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 1000, 593);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JButton NewEmployee = new JButton("");
+		NewEmployee.setBounds(82, 152, 95, 23);
+		contentPane.add(NewEmployee);
+		
+		JButton employeedetails = new JButton("");
+		employeedetails.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				EmployeeDetails det = new EmployeeDetails();
+				det.setVisible(true);
+				dispose();
+			}
+		});
+		employeedetails.setBounds(82, 207, 89, 29);
+		employeedetails.setBorderPainted(false);
+		employeedetails.setContentAreaFilled(false);
+		employeedetails.setOpaque(false);
+		contentPane.add(employeedetails);
+		
+		JButton employeeattendance = new JButton("");
+		employeeattendance.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				EmployeeAttendance att = new EmployeeAttendance();
+				att.setVisible(true);
+				dispose();
+			}
+		});
+		employeeattendance.setBounds(82, 270, 89, 29);
+		employeeattendance.setBorderPainted(false);
+		employeeattendance.setContentAreaFilled(false);
+		employeeattendance.setOpaque(false);
+		contentPane.add(employeeattendance);
+		
+		JButton overtime = new JButton("");
+		overtime.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Overtime ovr = new Overtime();
+				ovr.setVisible(true);
+				dispose();
+			}
+		});
+		overtime.setBounds(82, 331, 89, 23);
+		overtime.setBorderPainted(false);
+		overtime.setContentAreaFilled(false);
+		overtime.setOpaque(false);
+		contentPane.add(overtime);
+		
+		JButton leaverequest = new JButton("");
+		leaverequest.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				LeaveRequest leave = new LeaveRequest();
+				leave.setVisible(true);
+				dispose();
+			}
+		});
+		leaverequest.setBounds(82, 380, 95, 23);
+		leaverequest.setBorderPainted(false);
+		leaverequest.setContentAreaFilled(false);
+		leaverequest.setOpaque(false);
+		contentPane.add(leaverequest);
+		
+		JButton shifthours = new JButton("");
+		shifthours.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				EmployeesShiftHour sft = new EmployeesShiftHour();
+				sft.setVisible(true);
+				dispose();
+			}
+		});
+		shifthours.setBounds(82, 428, 89, 23);
+		shifthours.setBorderPainted(false);
+		shifthours.setContentAreaFilled(false);
+		shifthours.setOpaque(false);
+		contentPane.add(shifthours);
+		
+		JButton logout = new JButton("");
+		logout.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Homepage home = new Homepage();
+				home.setVisible(true);
+				dispose();
+			}
+		});
+		logout.setBounds(88, 511, 89, 23);
+		logout.setBorderPainted(false);
+		logout.setContentAreaFilled(false);
+		logout.setOpaque(false);
+		contentPane.add(logout);
+		
+		table = new JTable();
+		table.setBounds(249, 78, 700, 417);
+		contentPane.add(table);
+		
+		JLabel lblNewLabel = new JLabel("New label");
+		lblNewLabel.setIcon(new ImageIcon("C:\\Users\\HP\\Downloads\\6.png"));
+		lblNewLabel.setBounds(0, 0, 1000, 563);
+		contentPane.add(lblNewLabel);
+	}
+}
